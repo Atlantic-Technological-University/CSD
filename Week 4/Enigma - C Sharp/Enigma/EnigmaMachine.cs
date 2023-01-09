9889cd151ddf36d3321dd61c2555b55a5ac7bb96
konrad <konradpsiuk@gmail.com>
2023-01-09 20:15:13 +0000

    Revert "ApplyRotor method update"
    This reverts commit b97de9749ab7baada0f5b3ffcd48a41008313b9e.

--- Week 4/Enigma - C Sharp/Enigma/EnigmaMachine.cs
+++ Week 4/Enigma - C Sharp/Enigma/EnigmaMachine.cs
@@ -140,7 +140,6 @@ namespace Enigma
         private static string ApplyRotor(string message, string rotor)
         {
             // TO DO - add your implementation
-            //TODO Apply rotor method to go here somewhere in the future
             throw new NotImplementedException();
         }
 
