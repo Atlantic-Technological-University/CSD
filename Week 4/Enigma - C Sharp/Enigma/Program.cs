5f30bde5bf0228c9c36296bdbcce23dc92b42b5f
Keith Killilea <L00179804@atu.ie>
2025-01-13 20:36:12 +0000

    Example program

--- Week 4/Enigma - C Sharp/Enigma/Program.cs
+++ Week 4/Enigma - C Sharp/Enigma/Program.cs
@@ -11,29 +11,32 @@ namespace Enigma
     {
         static void Main(string[] args)
         {
-            //Changing the output encoding of the console to support '€' in output
-            string e = Encoding.Default.GetString(new byte[] { 128 });
-            Console.OutputEncoding = Encoding.Default;
-            Console.WriteLine(e);
+            // Changing the output encoding of the console to support '€' in output
+            Console.OutputEncoding = Encoding.UTF8;
 
             // List of rotors to map characters. Spaces and full stops are ignored.
-            List<string> rotors = new List<string>();
-            rotors.Add("BDFHJLCPRTXVZNYEIWGAKMUSQO");
-            
-            string startMessage = "Error messages are difficult to read";
+            List<string> rotors = new List<string>
+            {
+                "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+                "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+                "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+            };
+
+            string startMessage = "Error messages are difficult to read.";
 
             Console.WriteLine("The starting message is: " + startMessage);
 
-            string encodedMessage = EnigmaMachine.Encode(startMessage, 0, rotors);
+            // Encoding the message
+            string encodedMessage = EnigmaMachine.Encode(startMessage, 3, rotors);
             Console.WriteLine("\nThe encoded message is: {0}", encodedMessage);
 
+            // Decoding the message
             Console.WriteLine("\n\nDecoding the message: {0}", encodedMessage);
 
-            string decodedMessage = EnigmaMachine.Decode(encodedMessage, 0, rotors);
+            string decodedMessage = EnigmaMachine.Decode(encodedMessage, 3, rotors);
 
-            Console.WriteLine("\n\nThe decoded message is:{0} \n", decodedMessage);
-
+            Console.WriteLine("\n\nThe decoded message is: {0}\n", decodedMessage);
         }
-
     }
 }
+ 
